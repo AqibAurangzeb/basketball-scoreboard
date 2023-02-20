ac41e7c36cdca6a5eeb93f11c8cfefaa4ece297e
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-02-20 14:21:55 +0000

    changed board title from guest to away

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let homeScore = 0;
-let guestScore = 0;
+let awayScore = 0;
 
 let homeElement = document.getElementById("board-home-counter");
-let guestElement = document.getElementById("board-guest-counter");
+let awayElement = document.getElementById("board-away-counter");
 
 function increaseHomeScoreBy1() {
   homeScore += 1;
@@ -19,24 +19,24 @@ function increaseHomeScoreBy3() {
   homeElement.textContent = homeScore;
 }
 
-function increaseGuestScoreBy1() {
-  guestScore += 1;
-  guestElement.textContent = guestScore;
+function increaseAwayScoreBy1() {
+  awayScore += 1;
+  awayElement.textContent = awayScore;
 }
 
-function increaseGuestScoreBy2() {
-  guestScore += 2;
-  guestElement.textContent = guestScore;
+function increaseAwayScoreBy2() {
+  awayScore += 2;
+  awayElement.textContent = awayScore;
 }
 
-function increaseGuestScoreBy3() {
-  guestScore += 3;
-  guestElement.textContent = guestScore;
+function increaseAwayScoreBy3() {
+  awayScore += 3;
+  awayElement.textContent = awayScore;
 }
 
 function newGame() {
   homeScore = 0;
-  guestScore = 0;
+  awayScore = 0;
   homeElement.textContent = homeScore;
-  guestElement.textContent = guestScore;
+  awayElement.textContent = awayScore;
 }
